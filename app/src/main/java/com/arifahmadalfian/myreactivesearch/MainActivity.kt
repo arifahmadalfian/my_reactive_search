528abeaeb66cc3bcafeab4e256a6bd52388a3221
arifahmadalfian <arifahmadalfian@gmail.com>
2021-02-16 14:14:28 +0700

    Mengubah Main Activity

--- app/src/main/java/com/arifahmadalfian/myreactivesearch/MainActivity.kt
+++ app/src/main/java/com/arifahmadalfian/myreactivesearch/MainActivity.kt
@@ -2,10 +2,58 @@ package com.arifahmadalfian.myreactivesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import com.arifahmadalfian.myreactivesearch.databinding.ActivityMainBinding
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.launch
 
+@FlowPreview
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    @ExperimentalCoroutinesApi
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.edPlace.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                lifecycleScope.launch {
+                    viewModel.queryChannel.send(s.toString())
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        // ambil hasil pencarian dari model dan tampilkan autocompleteTextView
+        viewModel.searchResult.observe(this, Observer { placesItem ->
+            val placesName = arrayListOf<String?>()
+            placesItem.map {
+                placesName.add(it.placeName)
+            }
+            val adapter = ArrayAdapter(this@MainActivity, android.R.layout.select_dialog_item, placesName)
+            adapter.notifyDataSetChanged()
+            binding.edPlace.setAdapter(adapter)
+        })
+
     }
 }
