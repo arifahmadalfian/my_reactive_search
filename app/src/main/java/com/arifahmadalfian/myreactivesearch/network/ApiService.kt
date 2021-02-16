ab3eb6600aa719c47d0148a79c9f3f2862b236a1
arifahmadalfian <arifahmadalfian@gmail.com>
2021-02-16 13:53:19 +0700

    ApiService.kt dan ApiConfig.kt

---
+++ app/src/main/java/com/arifahmadalfian/myreactivesearch/network/ApiService.kt
@@ -0,0 +1,14 @@
+package com.arifahmadalfian.myreactivesearch.network
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("mapbox.places/{query}.json")
+    suspend fun getCountry(
+       @Path("query") query: String,
+       @Query("access_token") accessToken: String,
+       @Query("autocomplete") autoComplete: Boolean = true
+    )
+}
